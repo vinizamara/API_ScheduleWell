4b091f6a01ec8b919a06ef6453160785cf06cb9e
Alunods <migueldejf05@gmail.com>
2024-08-08 09:52:03 -0300

    inserção da função de desincriptar a senha

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,31 +1,41 @@
+const bcrypt = require("bcrypt");
 const connect = require("../db/connect"); // conexão com o banco
 
 module.exports = class LoginController {
-    static async postLogin(req, res) {
-        const { email, senha } = req.body;
-    
-        if (!email || !senha) {
-          return res.status(400).json({ error: "Email e Senha são obrigatórios" });
-        }
-    
-        const query = `SELECT * FROM Usuario WHERE email = '${email}' AND Senha = '${senha}'`;
-    
-        try {
-          connect.query(query, function (err, results) {
-            if (err) {
-              console.error(err);
-              return res.status(500).json({ error: "Erro interno do servidor" });
-            }
-    
-            if (results.length === 0) {
-              return res.status(401).json({ error: "Credenciais inválidas" });
-            }
-    
-            return res.status(200).json({ message: "Login realizado com sucesso", user: results[0] });
-          });
-        } catch (error) {
-          console.error("Erro ao executar a consulta:", error);
+  static async postLogin(req, res) {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+      return res.status(400).json({ error: "Email e Senha são obrigatórios" });
+    }
+
+    const query = "SELECT * FROM Usuario WHERE email = ?";
+
+    try {
+      connect.query(query, [email], async function (err, results) {
+        if (err) {
+          console.error(err);
           return res.status(500).json({ error: "Erro interno do servidor" });
         }
-      }    
+
+        if (results.length === 0) {
+          return res.status(401).json({ error: "Credenciais inválidas" });
+        }
+
+        const user = results[0];
+        
+        // Verifica se a senha enviada pelo usuário corresponde à senha armazenada
+        const isPasswordValid = await bcrypt.compare(senha, user.Senha);
+
+        if (!isPasswordValid) { 
+          return res.status(401).json({ error: "Credenciais inválidas" });
+        }
+
+        return res.status(200).json({ message: "Login realizado com sucesso", user });
+      });
+    } catch (error) {
+      console.error("Erro ao executar a consulta:", error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
 };
