a646de39abc2213fdd7f432ce0db7ea76fbc2f5f
Alunods <gabrielsantosmagalhaesx@gmail.com>
2024-09-10 15:39:31 -0300

    CRUD de anotações

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 const LoginController = require('../controller/loginController');
 const UserController = require('../controller/userController');
-const AgendaController = require('../controller/agendaController');
+const AnotacoesController = require('../controller/anotacoesController');
 const FinancasController = require('../controller/financasController');
 
 //Rotas do Usuario
@@ -11,14 +11,17 @@ router.get('/getUsers/', UserController.getUsers);
 router.put('/updateUser/:id', UserController.updateUser);
 router.delete('/deleteUser/:id', UserController.deleteUser);
 router.post('/login/', LoginController.postLogin);
-//Rotas de Agenda
-router.post('/postNota/', AgendaController.postNota);
-router.get('/getNota/', AgendaController.getNota);
-router.put('/updateNota/', AgendaController.updateNota);
-module.exports = router
+
+// Rotas de Anotações
+router.post('/postNota/', AnotacoesController.postNota);
+router.get('/getAnotacao/:idUsuario', AnotacoesController.getNota);
+router.put('/updateNota/', AnotacoesController.updateNota);
+router.delete('/deleteNota/:idAnotacao', AnotacoesController.deleteNota); 
 
 //Rotas de Anotações de Finanças
 router.post('/criarFinanca/', FinancasController.criarFinanca);
 router.get('/listarFinancas/:fk_id_usuario', FinancasController.listarFinancas);
 router.put('/atualizarFinanca/:id_financa', FinancasController.atualizarFinanca);
 router.delete('/deletarFinanca/:id_financa', FinancasController.deletarFinanca);
+
+module.exports = router;
